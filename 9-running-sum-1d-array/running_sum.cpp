48369ba9d429dae252c9e51f8ef027a292a4cb0d
Jason Armstrong <jasnarmstrng@gmail.com>
2022-12-28 23:53:47 +0000

    Add class solution with running_sum function

--- 9-running-sum-1d-array/running_sum.cpp
+++ 9-running-sum-1d-array/running_sum.cpp
@@ -7,6 +7,24 @@ Input: nums = [1,2,3,4]
 Output: [1,3,6,10]
 Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
 */
+#include <iostream>
+#include <vector>
+
+class Solution {
+  public:
+    std::vector<int> running_sum(const std::vector<int> nums) {
+      // calculate the running sum of the elements in array
+      // pre-conditions: Array size 1-1000, elements between -10^6 and 10^6
+      // post-conditions: return running some in vector. Don't modify original
+      std::vector<int> output;
+      int sum = 0;
+      for (int n: nums) {
+        sum += n;
+        output.push_back(sum);
+      }
+      return output;
+    }
+};
 
 int main() {
 
